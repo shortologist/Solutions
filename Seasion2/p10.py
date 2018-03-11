e16e8af3c823f81b6893ba615e7d8f12925c0a63
shortologist <islamatef82@gmail.com>
2018-03-11 05:15:12 +0200

    first commit

--- Seasion2/p10.py
+++
@@ -1,8 +0,0 @@
-n=int(input())
-while n!=0:
-    x=input()
-    if x[:2]=='on' or x[1:]=='ne' or x[0]+x[2]=='oe':
-        print(1)
-    else:
-        print(2 if x[:2]=='tw' or x[1:]=='wo' or x[0]+x[2]=='to' else 3)
-    n-=1
--- Seasion2/p8.py
+++ Seasion2/p8.py
@@ -1,17 +1,8 @@
-tu =('.', ',', '!', '?',' ')
-x=''
-def rEc(i,j):
-    if i>=j:
-        return True
-    if x[i]==x[j] or (x[i] in tu and x[j] in tu):
-        return rEc(i+1,j-1)
-    elif x[i] in tu:
-        return rEc(i+1,j)
-    elif x[j] in tu:
-        return rEc(i,j-1)
-    return False
-while True:
-    x=input().lower()
-    if x=='done':
-        break
-    print("You won't be eaten!"if rEc(0,len(x)-1) else 'Uh oh..')
+n=int(input())
+while n!=0:
+    x=input()
+    if x[:2]=='on' or x[1:]=='ne' or x[0]+x[2]=='oe':
+        print(1)
+    else:
+        print(2 if x[:2]=='tw' or x[1:]=='wo' or x[0]+x[2]=='to' else 3)
+    n-=1
